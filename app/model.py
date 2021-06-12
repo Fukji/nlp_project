a3fb99a48f6a4852e4b8c13713b077f0f6863384
Fukji <littlenubboy@gmail.com>
2021-06-12 19:11:56 +0700

    Added preprocessing

--- app/model.py
+++ app/model.py
@@ -1,20 +1,70 @@
 from fastai.text import *
-import numpy as np
 from utils import *
+import numpy as np
+import pandas as pd
+import os
 
 def load_model():
-    path = '/home/fukji/Documents/pytorch_deploy/nlp_project/app'
+    path = './app'
     learner = load_learner(path)
     return learner
 
 
+def load_dict():
+    alay_dict = pd.read_csv('./app/colloquial-indonesian-lexicon.csv', usecols=['slang', 'formal'])
+    alay_dict = dict(alay_dict.values)
+    belanja_dict = pd.read_csv('./app/kamus_belanja.csv', usecols=['slang', 'formal'])
+    belanja_dict = dict(belanja_dict.values)
+    return alay_dict, belanja_dict
+
+
 def preprocess(text):
-    return text
+    text = text.lower()
+    text = text.replace('[^\w\s]',' ')
+    text = text.replace('\s+', ' ')
+    text = text.replace(r'(.)\1+',r'\1\1')
+
+    tmp = text.split()
+    for i in range(len(tmp)):
+        flag = 0
+        if tmp[i][-3:] == 'nya':
+            tmp[i] = tmp[i][:-3]
+            flag = 1
+        
+        if tmp[i] in alay_dict:
+            tmp[i] = alay_dict[tmp[i]]
+            
+        if tmp[i] in belanja_dict:
+            tmp[i] = belanja_dict[tmp[i]]
+            
+        if flag:
+            tmp[i] += ' nya'
+
+    tmp = ' '.join(tmp)
+    text = tmp
+
+    text = text.replace('[^\w\s]',' ')
+    text = text.replace('\s+', ' ')
+    text = text.replace(r'(.)\1+',r'\1\1')
+
+    print(text)
+    print(len(text.split()))
+
+    if len(text.split()) <= 25:
+        return text
+
+    return None
+
+
 
 def get_prediction(text):
+    text = preprocess(text)
+    if text is None:
+        return '-1', '-1'
+
     prediction = learner.predict(preprocess(text))
     return prediction[1].numpy().tolist(), prediction[2].numpy().tolist()
-    return result
 
 
 learner = load_model()
+alay_dict, belanja_dict = load_dict()
